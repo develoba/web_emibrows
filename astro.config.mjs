499923989ede91177489c20a19523a1cb5edbcfa
develoba <srta.scobar@gmail.com>
2024-05-13 20:13:36 +0200

    feat: astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,47 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  image: {
-    // Example: Allow remote image optimization from a single domain
-    domains: ['astro.build'],
-  }
+  vite: {
+		build: {
+			cssMinify: "lightningcss",
+		},
+		ssr: {
+			noExternal: ["path-to-regexp"],
+		},
+		plugins: [
+			VitePWA({
+				registerType: "autoUpdate",
+				manifest,
+				workbox: {
+					globDirectory: ".vercel/output/static",
+					globPatterns: ["**/*.{html,js,css,woff,woff2,ttf,eot,ico}"],
+					runtimeCaching: [
+						{
+							urlPattern: /\.(?:png|jpg|jpeg|svg|gif|webp|avif)$/,
+							handler: "CacheFirst",
+							options: {
+								cacheName: "images",
+								expiration: {
+									maxEntries: 100,
+									maxAgeSeconds: 30 * 24 * 60 * 60,
+								},
+							},
+						},
+						{
+							urlPattern: /\.(?:woff|woff2|ttf|eot|ico)$/,
+							handler: "CacheFirst",
+							options: {
+								cacheName: "fonts",
+								expiration: {
+									maxEntries: 10,
+									maxAgeSeconds: 30 * 24 * 60 * 60,
+								},
+							},
+						},
+					],
+					navigateFallback: null,
+				},
+			}),
+		],
+	},
 });
